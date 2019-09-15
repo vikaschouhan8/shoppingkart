1cc42f7b963668804ca8c8da0afe2c623f7541f1
vikaschouhan8 <aboutvikaschouhan@gmai.com>
2019-09-15 21:37:29 +0530

    Add product detail functionality

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,32 +8,40 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 max-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={console.log("I ma on Image conatiner")}>
-                        <Link to="/details">
-                            <img src={img} alt="product" className="card-img-top" />
-                        </Link>
-
-
-
-
-                        <button
-                            className="cart-btn"
-                            disabled={inCart ? true : false}
-                        onClick={() => {console.log("Added to cart")}}
+                  <ProductConsumer>
+                    {
+                      (value)=>(
+                        <div className="img-container p-5" 
+                            onClick={
+                              ()=>value.handleDetail(id)
+                            }
                         >
-                            {inCart ? (<p className="text-capitalize mb-0" disabled>In cart</p>) :
-                                (<i className="fas fa-cart-plus" />
-                                )}
-                        </button>
-                    </div>
-                    {/* footer of cart */}
-                    <div className="card-footer d-flex justify-content-between">
-                        <p className="align-self-center mb-0">{title}</p>
-                        <h5 className="text-blue font-italic mb-0">
-                            <span className="mr-1">$</span>
-                            {price}
-                        </h5>
-                    </div>
+                            <Link to="/details">
+                                <img src={img} alt="product" className="card-img-top" />
+                            </Link>
+
+                            <button
+                                className="cart-btn"
+                                disabled={inCart ? true : false}
+                            onClick={() => {console.log("Added to cart")}}
+                            >
+                                {inCart ? (<p className="text-capitalize mb-0" disabled>In cart</p>) :
+                                    (<i className="fas fa-cart-plus" />
+                                    )}
+                            </button>
+                        </div>
+                      )
+                    }
+                  </ProductConsumer>
+
+                  {/* footer of cart */}
+                  <div className="card-footer d-flex justify-content-between">
+                      <p className="align-self-center mb-0">{title}</p>
+                      <h5 className="text-blue font-italic mb-0">
+                          <span className="mr-1">$</span>
+                          {price}
+                      </h5>
+                  </div>
                 </div>
             </ProductWrapper>
         );
